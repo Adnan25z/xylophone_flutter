8d84f47b216b06ac3933f61d6514ec3062ed91ac
Adnan <adnanzafar2001@gmail.com>
2025-07-15 21:22:43 +1000

    all working but repetitive code

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,121 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playsound(int noteNumber){
+    final player = AudioPlayer();
+    player.play(AssetSource('note$noteNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playsound(1);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  child: Container(), // or use SizedBox(height: 60)
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playsound(2);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  child: Container(), // or use SizedBox(height: 60)
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playsound(3);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  child: Container(), // or use SizedBox(height: 60)
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playsound(4);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  child: Container(), // or use SizedBox(height: 60)
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playsound(5);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  child: Container(), // or use SizedBox(height: 60)
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playsound(6);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.indigo,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  child: Container(), // or use SizedBox(height: 60)
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    playsound(7);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero,
+                    ),
+                  ),
+                  child: Container(), // or use SizedBox(height: 60)
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
